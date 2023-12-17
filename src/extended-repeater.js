38d16603ae771bc31f2c2adcaa5ee983fd7695a1
unknown <tatsiana.krauchanka@gmail.com>
2023-12-17 23:21:53 +0300

    feat: complete renameFiles task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,43 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
+function repeater(str, options) {
+/*   let newStr = "";
+
+  for (let j = 0; j < options.additionRepeatTimes; j++) {
+    if (!options.additionSeparator) {
+      options.additionSeparator = "|";
+    }
+    str +=
+      j === options.additionRepeatTimes - 1
+        ? options.addition
+        : options.addition + options.additionSeparator;
+  }
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    if (!options.separator) {
+      options.separator = "+";
+    }
+    newStr += i === options.repeatTimes - 1 ? str : str + options.separator;
+  }
+  return newStr; */
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
