0a9cd5288d42ad6c7e46905b49bf68218db1d833
unknown <tatsiana.krauchanka@gmail.com>
2023-12-23 01:04:33 +0300

    feat: complete minesweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -24,10 +24,97 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]
  */
 function minesweeper(matrix) {
-  throw new NotImplementedError('Not implemented');
+  const rows = matrix.length;
+  const columns = matrix[0].length;
+  let arr = [];
+  let count = 0;
+
+  for (let i = 0; i < rows; i++) {
+    let rowsArr = [];
+
+    for (let j = 0; j < columns; j++) {
+      //first row
+      if (i === 0) {
+        if (j === 0) {
+          matrix[i][j + 1] ? count++ : (count += 0);
+          matrix[i + 1][j] ? count++ : (count += 0);
+          matrix[i + 1][j + 1] ? count++ : (count += 0);
+        } else if (j === columns - 1) {
+          matrix[i][j - 1] ? count++ : (count += 0);
+          matrix[i + 1][j] ? count++ : (count += 0);
+          matrix[i + 1][j - 1] ? count++ : (count += 0);
+        } else {
+          matrix[i][j - 1] ? count++ : (count += 0); // left
+          matrix[i][j + 1] ? count++ : (count += 0); // right
+          matrix[i + 1][j] ? count++ : (count += 0); //
+          matrix[i + 1][j - 1] ? count++ : (count += 0);
+          matrix[i + 1][j + 1] ? count++ : (count += 0);
+        }
+        rowsArr.push(count);
+        count = 0;
+
+        //last row
+      } else if (i === rows - 1 && i !== 0) {
+        if (j === 0) {
+          matrix[i][j + 1] ? count++ : (count += 0);
+          matrix[i - 1][j] ? count++ : (count += 0);
+          matrix[i - 1][j + 1] ? count++ : (count += 0);
+        } else if (j === columns - 1) {
+          matrix[i][j - 1] ? count++ : (count += 0);
+          matrix[i - 1][j] ? count++ : (count += 0);
+          matrix[i - 1][j - 1] ? count++ : (count += 0);
+        } else {
+          matrix[i][j - 1] ? count++ : (count += 0); // left
+          matrix[i][j + 1] ? count++ : (count += 0); // right
+          matrix[i - 1][j] ? count++ : (count += 0); //
+          matrix[i - 1][j - 1] ? count++ : (count += 0);
+          matrix[i - 1][j + 1] ? count++ : (count += 0);
+        }
+        rowsArr.push(count);
+        count = 0;
+      } else {
+        //first column
+        if (j === 0) {
+          matrix[i][j + 1] ? count++ : (count += 0); // right
+          matrix[i - 1][j] ? count++ : (count += 0); //
+          matrix[i - 1][j - 1] ? count++ : (count += 0);
+          matrix[i + 1][j] ? count++ : (count += 0);
+          matrix[i + 1][j + 1] ? count++ : (count += 0);
+
+          //last column
+        } else if (j === columns - 1) {
+          matrix[i][j - 1] ? count++ : (count += 0); // left
+          matrix[i - 1][j] ? count++ : (count += 0); //
+          matrix[i - 1][j - 1] ? count++ : (count += 0);
+          matrix[i + 1][j] ? count++ : (count += 0);
+          matrix[i + 1][j + 1] ? count++ : (count += 0);
+        } else {
+          matrix[i][j - 1] ? count++ : (count += 0); // left
+          matrix[i][j + 1] ? count++ : (count += 0); // right
+          matrix[i - 1][j] ? count++ : (count += 0); //
+          matrix[i - 1][j - 1] ? count++ : (count += 0);
+          matrix[i - 1][j + 1] ? count++ : (count += 0);
+          matrix[i + 1][j] ? count++ : (count += 0);
+          matrix[i + 1][j + 1] ? count++ : (count += 0);
+          matrix[i + 1][j - 1] ? count++ : (count += 0);
+        }
+        rowsArr.push(count);
+        count = 0;
+      }
+    }
+    arr.push(rowsArr);
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    matrix[i].join(" ");
+  }
+
+  return arr;
+
+  /* throw new NotImplementedError('Not implemented'); */
   // remove line with error and write your code here
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
