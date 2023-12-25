e053ba57481db8f5a88642e5186471f84ef0fa51
unknown <tatsiana.krauchanka@gmail.com>
2023-12-25 21:33:06 +0300

    feat: complete vignere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,10 +20,14 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 class VigenereCipheringMachine {
+  constructor(val) {
+    this.val = val;
+  }
+
   alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   charWithNum = {};
 
-  fullObj() {
+  fillObj() {
     for (let i = 0; i < this.alphabet.length; i++) {
       let key = this.alphabet[i];
       this.charWithNum[key] = i;
@@ -32,25 +36,84 @@ class VigenereCipheringMachine {
   }
 
   encrypt(text, keyWord) {
-/*     this.fullObj();
-    let code = "";
-    let codeInNums = "";
-
-    for (let i = 0; i < text.length; i++) {
-      let numText = this.charWithNum[text[i]] + "  "; // получить цифру, которая соотв букве из объекта
-      let numKey = this.charWithNum[keyWord[i % keyWord.length]] + "  ";
-      codeInNums += ((numText + numKey) % this.alphabet.length) + " ";
-      code = this.alphabet[codeInNums];
+    if (
+      (arguments[0] !== false && arguments.length < 2) ||
+      arguments[0] === undefined ||
+      arguments[1] === undefined
+    ) {
+      throw new Error("Incorrect arguments!");
     }
 
-    return code; */
+    this.fillObj();
+    text = text.toUpperCase();
+    keyWord = keyWord.toUpperCase();
+    let code = "";
+    let numText = 0;
+    let numKey = 0;
+    let count = 0;
 
+    for (let i = 0; i < text.length; i++) {
+      numText = this.charWithNum[text[i]];
 
-    throw new NotImplementedError("Not implemented");
+      //проверка на пробелы, символы и тд
+      if (typeof numText !== "number") {
+        count++;
+        code += text[i];
+      } else {
+        numKey = this.charWithNum[keyWord[(i - count) % keyWord.length]];
+        code += this.alphabet[(numText + numKey) % this.alphabet.length];
+      }
+    }
+
+    if (this.val === false) {
+      code = code.split("").reverse().join("");
+    }
+
+    return code;
+
+    /* throw new NotImplementedError("Not implemented"); */
     // remove line with error and write your code here
   }
-  decrypt() {
-    throw new NotImplementedError("Not implemented");
+  decrypt(text, keyWord) {
+    if (
+      (arguments[0] !== false && arguments.length < 2) ||
+      arguments[0] === undefined ||
+      arguments[1] === undefined
+    ) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    this.fillObj();
+    text = text.toUpperCase();
+    keyWord = keyWord.toUpperCase();
+    let code = "";
+    let numText = 0;
+    let numKey = 0;
+    let count = 0;
+
+    for (let i = 0; i < text.length; i++) {
+      numText = this.charWithNum[text[i]];
+
+      //проверка на пробелы, символы и тд
+      if (typeof numText !== "number") {
+        count++;
+        code += text[i];
+      } else {
+        numKey = this.charWithNum[keyWord[(i - count) % keyWord.length]];
+        code +=
+          this.alphabet[
+            (numText - numKey + this.alphabet.length) % this.alphabet.length
+          ];
+      }
+    }
+
+    if (this.val === false) {
+      code = code.split("").reverse().join("");
+    }
+
+    return code;
+
+    /* throw new NotImplementedError("Not implemented"); */
     // remove line with error and write your code here
   }
 }
